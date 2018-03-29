f2e272c7b045c2b034c97366e12755ef5e695c79
tluo5458 <tluo5458@gmail.com>
2018-03-29 18:00:45 -0400

    Update TetrominoShape.java

--- TetrominoShape.java
+++ TetrominoShape.java
@@ -1,3 +1,21 @@
+import java.awt.Color;
+
 public enum TetrominoShape {
-	I, J, L, O, S, T, Z
+	I (new Color(3, 243, 241)),
+	J (new Color (3, 0, 235)),
+	L (new Color(246, 155, 1)),
+	O (new Color(237, 242, 6)),
+	S (new Color (5, 239, 7)),
+	T (new Color(164, 4, 229)),
+	Z (new Color(234, 4, 4));
+	
+	private Color color;
+	//int[][] points   double[] center of rotation  
+	private TetrominoShape(Color clr) {
+		color = clr;
+	}
+	
+	public Color getColor() {
+		return color;
+	}
 }
