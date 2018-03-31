4cc5ac511e65a0098c80b8dc815da12c62dd8eec
Ten086 <85halfnotes@gmail.com>
2018-03-30 20:08:52 -0400

    Update Tetromino

--- Tetronimo.java
+++ Tetronimo.java
@@ -1,70 +1,32 @@
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.image.BufferedImage;
 
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-
-public class Tetronimo {
+public class Tetromino {
 	
-	private TetrominoShape shape;
-	private Color color;
-	private int[][] coordinates;
+	TetrominoShape shape;
+	int[][] position = new int[4][2];
 	
-	public static final int boxSize = 20;
-	public static final Color lightBlue = new Color(3, 243, 241);
-	public static final Color darkBlue = new Color(3, 0, 235);
-	public static final Color orange = new Color(246, 155, 1);
-	public static final Color yellow = new Color(237, 242, 6);
-	public static final Color green = new Color(5, 239, 7);
-	public static final Color purple = new Color(164, 4, 229);
-	public static final Color red = new Color(234, 4, 4);
-
-	public static void main(String[] args) {
-		//this is just me being dumb and trying to draw stuff ignore
-		JFrame frame = new JFrame();
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setMinimumSize(new Dimension(500, 500));
-		TetrisPanel panel = new TetrisPanel();
-		frame.add(panel);
-		frame.setVisible(true);
-	}
-	
-	public Tetronimo(TetrominoShape s) {
+	public Tetromino(TetrominoShape s) {
 		shape = s;
-		assignColor();
+		position = shape.getCoords();
 	}
-
-	public void assignColor() {
-		switch (shape) {
-			case I:
-				color = lightBlue;
-				break;
-			case J:
-				color = darkBlue;
-				break;
-			case L:
-				color = orange;
-				break;
-			case O:
-				color = yellow;
-				break;
-			case S:
-				color = green;
-				break;
-			case T:
-				color = purple;
-				break;
-			case Z:
-				color = red;
-				break;
-			default: 
-				color = Color.magenta; //if something goes wrong it'll be very pink
-				break;
+	
+	public TetrominoShape getShape() {
+		return shape;
+	}
+	
+	public void printPos() {
+		for (int[] coords : position) {
+			System.out.print("row " + coords[0] + ", col " + coords[1] + " | ");
+		}
+	}
+	
+	public int[][] getPos() {
+		return position;
+	}
+	
+	public void changePos(int rShift, int cShift) {
+		for (int[] coord : position) {
+			coord[0] += rShift;
+			coord[1] += cShift;
 		}
 	}
 }
