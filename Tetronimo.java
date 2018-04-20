11dbc2119f39b00f16b03ec643ba77524c2e4e9c
Ten086 <85halfnotes@gmail.com>
2018-04-20 13:15:00 -0400

    Update Tetronimo.java

--- Tetronimo.java
+++ Tetronimo.java
@@ -2,8 +2,6 @@ public class Tetromino {
 	
 	TetrominoShape shape;
 	int[][] position = new int[4][2];
-	TetrominoShape[] shapes = {TetrominoShape.I, TetrominoShape.J, TetrominoShape.L,
-				TetrominoShape.O, TetrominoShape.S, TetrominoShape.T, TetrominoShape.Z};
 	
 	public Tetromino(TetrominoShape s) {
 		shape = s;
@@ -11,7 +9,8 @@ public class Tetromino {
 	}
 	
 	public Tetromino() {
-		
+		TetrominoShape[] shapes = {TetrominoShape.I, TetrominoShape.J, TetrominoShape.L,
+				TetrominoShape.O, TetrominoShape.S, TetrominoShape.T, TetrominoShape.Z};
 		int randomIndex = (int) (Math.random() * (shapes.length));
 		shape = shapes[randomIndex];
 		position = shape.getCoords();
@@ -41,7 +40,18 @@ public class Tetromino {
 		return setMino;
 	}
 	
+	public int[] getLowestCoord() {
+		int[] lowest = new int[2];
+		for (int[] coords : position) {
+			if (coords[0] > lowest[0]) {
+				lowest = coords;
+			}
+		}
+		return lowest;
+	}
+	
 	public void setInitialPos() {
+		position = shape.getCoords();
 		changePos(0, 4);
 	}
 	
@@ -51,4 +61,68 @@ public class Tetromino {
 			coord[1] += cShift;
 		}
 	}
+	
+	public boolean atBottom() {
+		boolean atBot = false;
+		for (int[] coord : position) {
+			if (coord[0] == Grid.ROWS - 1) {
+				atBot = true;
+			}
+		}
+		return atBot;
+	}
+	
+	public boolean collidesLeft(TetrisArray array) {
+		boolean collides = false;
+		for (int[] coord : position) {
+			for (Point p : Point.getSetMinoList()) {
+				int row = coord[0];
+				int col = coord[1];
+				if (col >= 0 && array.getPoint(row, col - 1) == p) {
+					collides = true;
+				}
+			}
+		}
+		return collides;
+	}
+	
+	public boolean collidesRight(TetrisArray array) {
+		boolean collides = false;
+		for (int[] coord : position) {
+			for (Point p : Point.getSetMinoList()) {
+				int row = coord[0];
+				int col = coord[1];
+				if (col < Grid.COLS && array.getPoint(row, col + 1) == p) {
+					collides = true;
+				}
+			}
+		}
+		return collides;
+	}
+	
+	public boolean collidesBot(TetrisArray array) {
+		boolean collides = false;
+		for (int[] coord : position) {
+			for (Point p : Point.getSetMinoList()) {
+				int row = coord[0];
+				int col = coord[1];
+				if (row < Grid.ROWS && array.getPoint(row - 1, col) == p) {
+					collides = true;
+				}
+			}
+		}
+		return collides;
+	}
+	/*public void rotate() {
+		int posLength = position.length;
+		int[] rows = new int[posLength];
+		int[] cols = new int[posLength];
+		for (int i = 0; i < posLength; i++) {
+			rows[i] = position[i][0];
+			cols[i] = position[i][1];
+			position[i][0] = cols[i];
+			position[i][1] = rows[i];
+		}
+	}*/
+	
 }
