ded2062b15b0e0c0e40e666239c29041fff3f744
tluo5458 <tluo5458@gmail.com>
2018-04-20 09:02:05 -0400

    center of rotation?
    trying the center of rotation thing?
    so like the coordinates for each tetromino is based on the center of rotation, and movement = move center of rotation, allows us to maybe handle rotation??
    idk pray

--- TetrominoShape.java
+++ TetrominoShape.java
@@ -1,13 +1,13 @@
 import java.awt.Color;
 
 public enum TetrominoShape {
-	I (new Color(3, 243, 241), "I", new int[][] {{0,0},{0,1},{0,2},{0,3}}),
-	J (new Color (3, 0, 235), "J", new int[][] {{0,1},{1,1},{2,1},{2,0}}),
-	L (new Color(246, 155, 1), "L", new int[][] {{0,0},{1,0},{2,0},{2,1}}),
-	O (new Color(237, 242, 6), "O", new int[][] {{0,0},{0,1},{1,0},{1,1}}),
-	S (new Color (5, 239, 7), "S", new int[][] {{1,0},{1,1},{0,1},{0,2}}),
-	T (new Color(164, 4, 229), "T", new int[][] {{0,0},{0,1},{0,2},{1,1}}),
-	Z (new Color(234, 4, 4), "Z", new int[][] {{0,0},{0,1},{1,1},{1,2}});
+	I (new Color(3, 243, 241), "I", new int[][] {{0,0},{0,1},{0,2},{0,3}}, new int[] {0.5, 1.5}),
+	J (new Color (3, 0, 235), "J", new int[][] {{0,1},{1,1},{2,1},{2,0}}, new int[] {1.0, 1.0}),
+	L (new Color(246, 155, 1), "L", new int[][] {{0,0},{1,0},{2,0},{2,1}}, new int[] {0.0, 1.0}),
+	O (new Color(237, 242, 6), "O", new int[][] {{0,0},{0,1},{1,0},{1,1}}, new int[] {0.5, 0.5}),
+	S (new Color (5, 239, 7), "S", new int[][] {{1,0},{1,1},{0,1},{0,2}}, new int[] {1.0, 1.0}),
+	T (new Color(164, 4, 229), "T", new int[][] {{0,0},{0,1},{0,2},{1,1}}, new int[] {0.0, 1.0}),
+	Z (new Color(234, 4, 4), "Z", new int[][] {{0,0},{0,1},{1,1},{1,2}, new int[] {0.0, 1.0}});
 	
 	private Color color;
 	private String name;
@@ -15,10 +15,11 @@ public enum TetrominoShape {
 	private int[][] coords;
 	private double[] COR; 
 	
-	private TetrominoShape(Color clr, String n, int[][] c) {
+	private TetrominoShape(Color clr, String n, int[][] c, double[] cor) {
 		color = clr;
 		name = n;
 		coords = c;
+		COR = cor;
 	}
 	
 	public Color getColor() {
