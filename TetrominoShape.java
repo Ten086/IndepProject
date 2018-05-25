6733313787c8fbad192828c4a8570b224adefd95
tluo5458 <tluo5458@gmail.com>
2018-05-25 02:46:34 -0400

    Final

--- TetrominoShape.java
+++ TetrominoShape.java
@@ -1,45 +1,45 @@
 import java.awt.Color;
 
 public enum TetrominoShape {
-	I (new Color(3, 243, 241), "I", new int[][] {{0,0},{0,1},{0,2},{0,3}}, new double[] {0.5, 1.5}),
-	J (new Color (3, 0, 235), "J", new int[][] {{0,0},{1,0},{1,1},{1,2}}, new double[] {1.0, 1.0}),
-	L (new Color(246, 155, 1), "L", new int[][] {{1,0},{1,1},{1,2},{0,2}}, new double[] {1.0, 1.0}),
-	O (new Color(237, 242, 6), "O", new int[][] {{0,0},{0,1},{1,0},{1,1}}, new double[] {0.5, 0.5}),
-	S (new Color (5, 239, 7), "S", new int[][] {{1,0},{1,1},{0,1},{0,2}}, new double[] {1.0, 1.0}),
-	T (new Color(164, 4, 229), "T", new int[][] {{1,0},{1,1},{1,2},{0,1}}, new double[] {1.0, 1.0}),
-	Z (new Color(234, 4, 4), "Z", new int[][] {{0,0},{0,1},{1,1},{1,2}}, new double[] {1.0, 1.0});
-	
+	I (Grid.LIGHTBLUE, "I", new int[][] {{0,0},{0,1},{0,2},{0,3}}, new double[] {0.5, 1.5}),
+	J (Grid.DARKBLUE, "J", new int[][] {{0,0},{1,0},{1,1},{1,2}}, new double[] {1.0, 1.0}),
+	L (Grid.ORANGE, "L", new int[][] {{1,0},{1,1},{1,2},{0,2}}, new double[] {1.0, 1.0}),
+	O (Grid.YELLOW, "O", new int[][] {{0,0},{0,1},{1,0},{1,1}}, new double[] {0.5, 0.5}),
+	S (Grid.GREEN, "S", new int[][] {{1,0},{1,1},{0,1},{0,2}}, new double[] {1.0, 1.0}),
+	T (Grid.PURPLE, "T", new int[][] {{1,0},{1,1},{1,2},{0,1}}, new double[] {1.0, 1.0}),
+	Z (Grid.RED, "Z", new int[][] {{0,0},{0,1},{1,1},{1,2}}, new double[] {1.0, 1.0});
+
 	private Color color;
 	private String name;
-	
+
 	private int[][] coords;
 	private double[] COR; 
-	
+
 	private TetrominoShape(Color clr, String n, int[][] c, double[] cor) {
 		color = clr;
 		name = n;
 		coords = c;
 		COR = cor;
 	}
-	
+
 	public Color getColor() {
 		return color;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
-	
+
 	public int[][] getCoords() {
 		return coords;
 	}
-	
+
 	public String toString() {
 		return name;
 	}
-	
+
 	public double[] getCOR() {
 		return COR;
 	}
-	
+
 }
